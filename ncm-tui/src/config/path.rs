9130fd93d629481f6f07ecfc69e79360662bfe19
jlb <light2253@qq.com>
2024-12-07 18:45:20 +0800

    尝试修复文件夹创建错误

--- ncm-tui/src/config/path.rs
+++ ncm-tui/src/config/path.rs
@@ -16,26 +16,41 @@ pub struct Path {
 
 impl Path {
     pub fn new() -> Self {
-        let data = dirs_next::data_dir().unwrap().join(APP_NAME);
+        // 获取并验证各个目录路径
+        let data = dirs_next::data_dir().unwrap_or_else(|| {
+            panic!("Could not retrieve the data directory.");
+        }).join(APP_NAME);
         if !data.exists() {
-            fs::create_dir(&data).expect("Couldn't create data dir.");
+            fs::create_dir(&data).unwrap_or_else(|e| {
+                panic!("Couldn't create data dir at {:?}: {}", data, e);
+            });
         }
 
-        let config = dirs_next::config_dir().unwrap().join(APP_NAME);
+        let config = dirs_next::config_dir().unwrap_or_else(|| {
+            panic!("Could not retrieve the config directory.");
+        }).join(APP_NAME);
         if !config.exists() {
-            fs::create_dir(&config).expect("Couldn't create config dir.");
+            fs::create_dir(&config).unwrap_or_else(|e| {
+                panic!("Couldn't create config dir at {:?}: {}", config, e);
+            });
         }
 
-        let cache = dirs_next::cache_dir().unwrap().join(APP_NAME);
+        let cache = dirs_next::cache_dir().unwrap_or_else(|| {
+            panic!("Could not retrieve the cache directory.");
+        }).join(APP_NAME);
         if !cache.exists() {
-            fs::create_dir(&cache).expect("Couldn't create cache dir.");
+            fs::create_dir(&cache).unwrap_or_else(|e| {
+                panic!("Couldn't create cache dir at {:?}: {}", cache, e);
+            });
         }
 
         let login_cookie = data.clone().join("cookies.json");
 
         let lyrics = data.clone().join("lyrics");
         if !lyrics.exists() {
-            fs::create_dir(&lyrics).expect("Couldn't create lyrics dir.");
+            fs::create_dir(&lyrics).unwrap_or_else(|e| {
+                panic!("Couldn't create lyrics dir at {:?}: {}", lyrics, e);
+            });
         }
 
         Self {
